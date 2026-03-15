dd6e649fdf9f1a754c011a64f95508a955a98952
Nadeem Mir <nadeemmir873@gmail.com>
2026-03-15 06:20:26 +0530

    Some last fixes for deployment

--- main.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-text = "RAG systems combine retrieval and generation"
-
-embedding = model.encode(text)
-
-print("Embedding length:", len(embedding))
